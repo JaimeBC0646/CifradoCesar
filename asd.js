a3a658c5ec42fe264698a5cd9521e9b468a3cd1e
JaimeBC0646 <147665532+JaimeBC0646@users.noreply.github.com>
2023-10-12 00:00:44 -0600

    Update asd.js
    methods of Cesar cipher, updated

--- asd.js
+++ asd.js
@@ -1,9 +1,10 @@
 function encryptWord(){
     var word = document.getElementById("txtWordEncrypt").value;
-    var abc= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    var abc= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     var letter = '';
     var cad = "";
-
+    //alert(abc)
+    //alert(abc.length/2 +" "+abc.length)
     for(let i = 0; i<word.length ; i++)
     {
         letter = word.at(i) //Se guarda el valor de la letra
@@ -15,12 +16,9 @@ function encryptWord(){
             }
             else if(letter == abc[x]) //letra = una letra en el abc?
             {
-                if(x>23 && x<27) //posicion de la letra excede a 25?
+                if(x>24 && x<27 || x>51 && x<54) //posicion de la letra excede a 25?
                 {
-                    cad = cad + abc[x+2-abc.length]; //Minuscula se suma a la cadena ->tamaño de abecedario - la posicion de la letra (resta -1 por el arreglo)
-                }
-                else if(x>50 && x<54){
-                    cad = cad + abc[x+2-abc.length/2]; //Mayuscula se suma a la cadena ->tamaño de abecedario - la posicion de la letra (resta -1 por el arreglo)
+                    cad = cad + abc[x+2-(abc.length/2)]; //Minuscula se suma a la cadena ->tamaño de abecedario - la posicion de la letra (resta -1 por el arreglo)
                 }
                 else{
                     cad = cad + (abc[x+2]); //se suma a la cadena -> la letra cifrada de la posicion
@@ -35,7 +33,8 @@ function encryptWord(){
 
 function dcryptWord(){
     var word = document.getElementById("txtWordDecrypt").value;
-    var abc= ['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a','Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K','J','I','H','G','F','E','D','C','B','A'];
+
+    var abc= ['z','y','x','w','v','u','t','s','r','q','p','o','ñ','n','m','l','k','j','i','h','g','f','e','d','c','b','a','Z','Y','X','W','V','U','T','S','R','Q','P','O','Ñ','N','M','L','K','J','I','H','G','F','E','D','C','B','A'];
     var letter = '';
     var cad = "";
 
@@ -50,12 +49,9 @@ function dcryptWord(){
             }
             else if(letter == abc[x]) //letra = una letra en el abc?
             {
-                if(x>23 && x<27) //posicion de la letra excede a 25?
+                if(x>24 && x<27 || x>51 && x<54) //posicion de la letra excede a 25?
                 {
-                    cad = cad + abc[x+2-abc.length]; //se suma a la cadena ->tamaño de abecedario - la posicion de la letra (resta -1 por el arreglo)
-                }
-                else if(x>50 && x<54){
-                    cad = cad + abc[x+2-abc.length/2]; //se suma a la cadena ->tamaño de abecedario - la posicion de la letra (resta -1 por el arreglo)
+                    cad = cad + abc[x+2-(abc.length/2)]; //Minuscula se suma a la cadena ->tamaño de abecedario - la posicion de la letra (resta -1 por el arreglo)
                 }
                 else{
                     cad = cad + (abc[x+2]); //se suma a la cadena -> la letra cifrada de la posicion
